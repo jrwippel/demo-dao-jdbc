3c09eda3994a8367575c3c47721b71a524791f7c
Jackson R Wippel <jrwippel@hotmail.com>
2022-01-06 07:45:07 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/main/java/model/entities/SellerDao.java
@@ -0,0 +1,12 @@
+package model.entities;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert (Seller obj);
+    void update (Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
