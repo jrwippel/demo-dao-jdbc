cb02f2dc2ae4700fefffd907630440f012a4b659
Jackson R Wippel <jrwippel@hotmail.com>
2022-01-06 07:34:37 -0300

    Seller entity class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,12 +2,18 @@ package application;
 
 import com.sun.source.tree.NewArrayTree;
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main (String[] args){
 
         Department department = new Department(1, "Books");
-        System.out.println(department);
+
+        Seller seller = new Seller(7, "Jackson Wippel", "jrwippel@hotmail.com", new Date(),1000.00, department);
+
+        System.out.println(seller);
     }
 }
